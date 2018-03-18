d0dd5cc3d3d080b68420ca98299eaf34da9a8d80
Slex Atroukov <>
2018-03-18 22:02:29 +0000

    refactored connect to rerender when store changes

--- packages/react-slex-store/src/connect.js
+++ packages/react-slex-store/src/connect.js
@@ -1,12 +1,34 @@
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
+import shallowEquals from './shallowEquals'
 
 function connect (fn) {
   return WrappedComponent => {
     class Connected extends Component {
+      constructor (props, context) {
+        super(props, context)
+        this.store = props.store || context.store
+      }
+      componentDidMount () {
+        this.unsubscribe = this.store.subscribe(state => {
+          this.hasStoreStateChanged = true
+          this.forceUpdate()
+        })
+      }
+      componentWillUnmount () {
+        this.unsubscribe && this.unsubscribe()
+        this.unsubscribe = undefined
+      }
+      componentWillReceiveProps (nextProps) {
+        this.hasPropsStateChanged = !shallowEquals(this.props, nextProps)
+      }
+      shouldComponentUpdate () {
+        return this.hasStoreStateChanged || this.hasPropsStateChanged
+      }
       render () {
-        const store = this.props.store || this.context.store
-        const nextProps = fn(store.dispatch, store.getState, this.props)
+        const nextProps = fn(this.store.dispatch, this.store.getState, this.props)
+        this.hasStoreStateChanged = false
+        this.hasPropsStateChanged = false
         return <WrappedComponent {...nextProps} />
       }
     }
