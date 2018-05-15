835615e8486d198b5af0526d400c5472428375bc
Slex Atroukov <>
2018-05-15 07:40:47 +0100

    removed editable

--- packages/react-slex-ui/src/Form/index.js
+++ packages/react-slex-ui/src/Form/index.js
@@ -1,12 +1,10 @@
 import Dropdown from './Dropdown'
-import Editable from './Editable'
 import FieldSet from './FieldSet'
 import Icon from './Icon'
 import Input from './Input'
 
 export {
   Dropdown,
-  Editable,
   FieldSet,
   Icon,
   Input
